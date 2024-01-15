4b006cb5ee8e470be6646ce0cef1e6218fbb26ba
Pattrapol.J <pattrapol.jam@gmail.com>
2024-01-15 23:30:15 +0700

    server_error_screen

--- lib/social_login/server_down_screen.dart
+++ lib/social_login/server_down_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mun_bot/env.dart';
 import 'package:mun_bot/util/size_config.dart';
 
 class ServerDownScreen extends StatelessWidget {
@@ -6,6 +7,7 @@ class ServerDownScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return SafeArea(
       child: Container(
         width: SizeConfig.screenWidth,
@@ -14,20 +16,42 @@ class ServerDownScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/images/ErrorNetwork.png"),
-              Text("TESTING SERVER DOWN"),
-              ElevatedButton(
-                  onPressed: () =>
-                      {Navigator.pop(context), Navigator.pop(context)},
-                  child: Row(
-                    children: [
-                      Icon(Icons.backspace),
-                      Text(
+              SizedBox(
+                  width: 400,
+                  height: 400,
+                  child: Image.asset("assets/images/server_error.png")),
+              // Text("TESTING SERVER DOWN"),
+              InkWell(
+                splashColor: Colors.yellow, // Customize the splash color
+                highlightColor: Colors.orange,
+                borderRadius: BorderRadius.circular(sizeHeight(15, context)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(sizeHeight(15,
+                          context)), // Adjust the value for different border radii
+                      gradient: LinearGradient(
+                        colors: [
+                          theme_color,
+                          theme_color2
+                        ], // Customize your gradient colors
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                    width: sizeWidth(250, context),
+                    height: sizeHeight(52, context),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
                         "Back to Login Screen",
                         style: TextStyle(fontSize: sizeHeight(20, context)),
                       ),
-                    ],
-                  ))
+                    )),
+              ),
             ],
           ),
         ),
